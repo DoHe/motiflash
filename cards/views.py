5040aedc0d764a8e04beaa7c692c5bad52184d2d
dohe <dominik.henter@ecosia.org>
2021-04-13 11:31:22 +0200

    working

--- cards/views.py
+++ cards/views.py
@@ -2,7 +2,7 @@ from django.core.serializers import serialize
 from django.http import JsonResponse
 from django.views import View
 from django.views.generic import TemplateView, FormView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse
 
 from cards.models import Card, Course
 from cards.forms import CourseForm, CardForm
@@ -23,7 +23,7 @@ class Cards(TemplateView):
             Card.objects.filter(course=self.course).all().values()
         )
         if not self.cards:
-            return redirect('cards_add')
+            return redirect(f"{reverse('cards_add')}?course={self.course}")
 
         return super().get(request)
 
@@ -38,12 +38,24 @@ class Cards(TemplateView):
 class CardAddView(FormView):
     template_name = "card_add.html"
     form_class = CardForm
-    success_url = "."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course'] = Course.objects.get(
+            id=self.request.GET.get('course')
+        )
+        return context
 
     def form_valid(self, form):
         card = Card(**form.cleaned_data)
         card.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        params = self.request.GET.copy()
+        form = self.get_form()
+        params['success'] = form['term'].value()
+        return f'.?{params.urlencode()}'
 
 
 class Courses(FormView):
