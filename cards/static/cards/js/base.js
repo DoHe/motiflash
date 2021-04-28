d7629059aa5e54d6032df33274f4a7766a824739
dohe <dominik.henter@ecosia.org>
2021-04-28 09:05:10 +0200

    Add notifications capabilities

--- cards/static/cards/js/base.js
+++ cards/static/cards/js/base.js
@@ -27,7 +27,38 @@ function initModal() {
   }
 }
 
+function initNotifications() {
+  const $notifications = Array.prototype.slice.call(document.querySelectorAll('.notification'), 0);
+  $notifications.forEach(notification => {
+    notification.querySelector('.js-delete').addEventListener('click', () => {
+      notification.parentNode.removeChild(notification);
+    })
+  });
+}
+
+function checkNotifications() {
+  fetch('/notifications')
+}
+
+function showNotification(text) {
+  const template = document.createElement('template');
+  template.innerHTML = `<div class="notification is-primary is-light">
+    <button class="delete js-delete"></button>
+    ${text}
+  </div>`
+  const notification = template.content.firstChild;
+  notification.querySelector('.js-delete').addEventListener('click', () => {
+    notification.parentNode.removeChild(notification);
+  })
+  const notificationArea = document.querySelector('.js-achievement');
+  if (notificationArea) {
+    notificationArea.appendChild(notification);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     initNavbar();
     initModal();
+    initNotifications();
+    window.showNotification = showNotification;
 });
